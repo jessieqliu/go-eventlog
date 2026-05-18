6c594047da24fe61a38f628224ad527d46df348b
Jessie Liu <jessieqliu@google.com>
2026-05-18 18:06:09 +0000

    gofmt

--- extract/extract_test.go
+++ extract/extract_test.go
@@ -896,7 +896,7 @@ func getEventsFromLog(t *testing.T, events []tcg.Event) []tcg.Event {
 		} else {
 			// First event for this PCR - initialize with zeros.
 			// Note this is a simplification for some PCRs. PCRs 17-23 are initialized with 0xFF but
-			// the DRTM event clears the index to 0x00 before extending. Starting with 0x00 is functionally 
+			// the DRTM event clears the index to 0x00 before extending. Starting with 0x00 is functionally
 			// the same because DRTM is always the first event, but this is subtly different from the spec.
 			initial := make([]byte, h.Size())
 			if e.Type == tcg.EFIHCRTMEvent {
